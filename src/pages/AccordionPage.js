548ad61823573e74444e61b46a74f52b8bc83aae
mohamedagina <mohamedagina001@gmail.com>
2023-03-07 23:09:32 +0200

    feat: Create Accordion Page

---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,31 @@
+import Accordion from '../components/Accordion';
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 'dsaas',
+      label: 'Do you like React js ?',
+      content:
+        'Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. '
+    },
+    {
+      id: 'dfsds',
+      label: 'Do you like Javascript ?',
+      content:
+        'Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. '
+    },
+    {
+      id: 'hfdfvs',
+      label: 'Do you like HTML ?',
+      content:
+        'Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. Yes, you can use it to create awesome web apps. '
+    }
+  ];
+  return (
+    <div className="App">
+      <Accordion items={items} />
+    </div>
+  );
+}
+
+export default AccordionPage;
