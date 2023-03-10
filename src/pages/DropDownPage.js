e4d5c9800419bfb64a9b4c2167ae9e9502cb38b1
mohamedagina <mohamedagina001@gmail.com>
2023-03-11 00:04:00 +0200

    feat: create DropDownPage

---
+++ src/pages/DropDownPage.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import DropDown from './components/DropDown';
+
+function DropDownPage() {
+  const [selected, setSelected] = useState();
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Blue', value: 'blue' },
+    { label: 'Green', value: 'green' }
+  ];
+
+  const handleChange = option => {
+    setSelected(option);
+  };
+  return (
+    <div>
+      <DropDown options={options} value={selected} onChange={handleChange} />
+    </div>
+  );
+}
+
+export default DropDownPage;
