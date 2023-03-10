6b20c44a7ba69e3274597fc0c0c15f2af0c3f9d6
mohamedagina <mohamedagina001@gmail.com>
2023-03-11 00:53:40 +0200

    feat: Use our navigation system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Route from './components/Route';
+
+import SideBar from './components/SideBar';
+
+import DropDownPage from './pages/DropDownPage';
+import AccordionPage from './pages/AccordionPage';
+import ButtonsPage from './pages/ButtonsPage';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <SideBar />
+      <div className="col-span-5">
+        <Route path="/">
+          <DropDownPage />
+        </Route>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonsPage />
+        </Route>
+      </div>
+    </div>
+  );
 }
 
 export default App;
